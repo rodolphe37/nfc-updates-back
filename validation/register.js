03868ca848d6d45f42476527c3076ff0f2f85950
dylan belouis <dylan.belouis@gmail.com>
2020-01-08 10:52:38 +0100

    ajout create with validator and JWT

--- validation/register.js
+++ validation/register.js
@@ -8,7 +8,6 @@ module.exports = function validateRegisterInput(data) {
   data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
   
   // Name checks
   if (Validator.isEmpty(data.name)) {
@@ -27,16 +26,8 @@ module.exports = function validateRegisterInput(data) {
     errors.password = "Password field is required";
   }
   
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm password field is required";
-  }
-  
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
-  }
-  
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
   }
   
   return {
