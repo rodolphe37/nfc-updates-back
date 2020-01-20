2aab34ec00c740bb1560e77a01325b9fdcbaf037
rodolphe37 <rodolphe.a@gmail.com>
2020-01-20 11:35:15 +0100

    Add save password and hash this on the fly when the edit user is used

--- routes/users.js
+++ routes/users.js
@@ -1,110 +1,113 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-const bcrypt = require("bcryptjs");
-const connection = require("../config/connection");
+const connection = require('../config/connection');
 
 
-const validateRegisterInput = require("../validation/register");
-const validateLoginInput = require("../validation/login");
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
 
 
-const createUser = async ({ name, password, company, phone, email, id }) => {
-  return await User.create({ name, password, company, phone, email, id });
-};
-
-
+// const createUser = async ({
+//   name, password, company, phone, email, id,
+// }) => await User.create({
+//   name, password, company, phone, email, id,
+// });
 
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
-    // Form validation
-    
-    const { errors, isValid } = validateRegisterInput(req.body);
-    
-    // Check validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
-    
-    connection.query(`SELECT email FROM users WHERE email = ?`,[req.body.email], (err, results) => {
-        if (err) {
-          res.status(500).send("Email already exists" );
-        } else {
-          //res.json(results);
-          const newUser = new User({
-            id: req.params.id,
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            company: req.body.company,
-            phone: req.body.phone 
-          });
-          // Hash password before saving in database
-        bcrypt.genSalt(10, (err, salt,  ) => {
-            bcrypt.hash(new User.password, salt, (err, hash) => {
-              if (err);
-              newUser.password = hash;
-              newUser
-                .save()
-                .then(results => res.json(results))
-                .catch(err => console.log(err));
-            });
-          });
-        }
-      });
-  });
-
-
-router.post("/", async (req, res) => {
+router.post('/register', (req, res) => {
   // Form validation
-  const id = req.params.id
-  const { name, password, company, phone, email  } = req.body;
+  const { id } = req.params;
+  const {
+    name, password, company, phone, email,
+  } = req.body;
 
   const { errors, isValid } = validateRegisterInput(req.body);
-  
+
   // Check validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
- else {
-  const newUser = new User({
-    id: req.params.id,
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    company: req.body.company,
-    phone: req.body.phone 
-  });
-      // Hash password before saving in database
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err);
-        newUser.password = hash;
-        newUser
-          .save()
-          .then(results => res.json(results))
-          .catch(err => console.log(err));
-      });
-    });
-  };
-});
-    
- 
 
+  connection.query('SELECT email FROM users WHERE email = ?', [email], (err, results) => {
+    if (err) {
+      res.status(500).send('Email already exists');
+    } else {
+      // res.json(results);
+      const newUser = new User({
+        id,
+        name,
+        email,
+        password,
+        company,
+        phone,
+      });
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(new User.Password(), salt, (err, hash) => {
+          if (err);
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((results) => res.json(results))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+
+router.post('/', async (req, res) => {
+  // Form validation
+  const { id } = req.params;
+  const {
+    name, password, company, phone, email,
+  } = req.body;
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const newUser = new User({
+    id,
+    name,
+    email,
+    password,
+    company,
+    phone,
+  });
+  // Hash password before saving in database
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err);
+      newUser.password = hash;
+      newUser
+        .save()
+        .then((results) => res.json(results))
+        .catch((err) => console.log(err));
+    });
+  });
+});
 
 
 router.get('/', async (req, res) => {
   const {
-    _start, _end, _order, _sort, q
+    _start, _end, _order, _sort, q,
   } = req.query;
   try {
     const { count, rows } = await User.findAndCountAll({
-      q: q,
+      q,
       limit: _end - _start,
       offset: Number(_start),
       order: [
@@ -138,8 +141,6 @@ router.get('/:id', async (req, res) => {
 });
 
 
-
-
 router.delete('/:id', async (req, res) => {
   await User.destroy({
     where: {
@@ -157,23 +158,42 @@ router.delete('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   const {
-    name, email, phone, company,
+    id, name, email, phone, company, password,
   } = req.body;
   const { errors, isValid } = validateLoginInput(req.body);
-    
+
   // Check validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
   await User.update(
     {
-      name, email, phone, company,
+      id, name, email, phone, company, password,
     },
     { where: { id: req.params.id } },
   )
     .then(() => {
       res.json({ status: 'User Updated!' });
     });
+  const newUser = new User({
+    id,
+    name,
+    email,
+    password,
+    company,
+    phone,
+  });
+  // Hash password before saving in database
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err);
+      newUser.password = hash;
+      newUser
+        .save()
+        .then((results) => res.json(results))
+        .catch((err) => console.log(err));
+    });
+  });
 });
 
 module.exports = router;
